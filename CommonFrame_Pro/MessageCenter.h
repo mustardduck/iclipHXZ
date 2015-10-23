28408aa2535cace3d291b14c87be58fd9e8b410c
qian mo <mustardduck@qq.com>
2015-10-23 21:30:04 +0800

    lijun local url

---
+++ CommonFrame_Pro/MessageCenter.h
@@ -0,0 +1,13 @@
+//
+//  MessageCenter.h
+//  CommonFrame_Pro
+//
+//  Created by 倩 莫 on 15/10/23.
+//  Copyright © 2015年 ionitech. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MessageCenter : NSObject
+
+@end
