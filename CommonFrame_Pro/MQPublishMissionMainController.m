c31fdcda46143320070ab6b672a2e1d054a6505b
qian mo <mustardduck@qq.com>
2015-11-11 15:30:42 +0800

    fix delete zi renwu

--- CommonFrame_Pro/MQPublishMissionMainController.m
+++ CommonFrame_Pro/MQPublishMissionMainController.m
@@ -820,9 +820,14 @@
 {
     if (editingStyle == UITableViewCellEditingStyleDelete)
     {
-        UIAlertView* alert = [[UIAlertView alloc] initWithTitle:@"" message:[NSString stringWithFormat:@"是否删除该任务？"] delegate:self cancelButtonTitle:@"取消" otherButtonTitles:@"确定", nil];
-        alert.tag = indexPath.row;
-        [alert show];
+        [self hiddenKeyboard];
+        dispatch_time_t time=dispatch_time(DISPATCH_TIME_NOW, 0.5*NSEC_PER_SEC);
+        dispatch_after(time, dispatch_get_main_queue(), ^{
+            
+            UIAlertView* alert = [[UIAlertView alloc] initWithTitle:@"" message:[NSString stringWithFormat:@"是否删除该任务？"] delegate:self cancelButtonTitle:@"取消" otherButtonTitles:@"确定", nil];
+            alert.tag = indexPath.row;
+            [alert show];
+        });
     }
 }
 
