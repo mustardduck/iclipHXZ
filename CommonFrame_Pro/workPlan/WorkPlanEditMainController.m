69ea1c87c7170d2e59f9716e03285ab08149a460
qian mo <mustardduck@qq.com>
2016-02-26 11:52:54 +0800

    fix ui for planedit

--- CommonFrame_Pro/workPlan/WorkPlanEditMainController.m
+++ CommonFrame_Pro/workPlan/WorkPlanEditMainController.m
@@ -1989,4 +1989,13 @@
     }
 }
 
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView {
+    CGFloat sectionHeaderHeight = 54;
+    if (scrollView.contentOffset.y<=sectionHeaderHeight&&scrollView.contentOffset.y>=0) {
+        scrollView.contentInset = UIEdgeInsetsMake(-scrollView.contentOffset.y, 0, 0, 0);
+    } else if (scrollView.contentOffset.y>=sectionHeaderHeight) {
+        scrollView.contentInset = UIEdgeInsetsMake(-sectionHeaderHeight, 0, 0, 0);
+    }
+}
+
 @end
