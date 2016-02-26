2e9b09d6fe8a79bb22c3e6b212b40a7356fb3eb0
qian mo <mustardduck@qq.com>
2016-02-26 15:35:36 +0800

    fix 7

--- CommonFrame_Pro/CDSideBarController/ICSideMenuController.m
+++ CommonFrame_Pro/CDSideBarController/ICSideMenuController.m
@@ -487,7 +487,9 @@
                         [self addBadgeView:menuView parentView:menuView  showValue:allnumBadgeValueString isAllNum:YES withIndex:index];
                     }
                     
-                    UIButton *menuButton = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, W(menuView), H(menuView))];
+//                    UIButton *menuButton = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, W(menuView), H(menuView))];
+                    UIButton *menuButton = [[UIButton alloc] initWithFrame:CGRectMake((menuWidth - 60) / 2, 5, 60, 60)];
+
                     [menuButton setBackgroundColor:[UIColor clearColor]];
                     [menuButton addTarget:self action:@selector(menuButtonClicked:) forControlEvents:UIControlEventTouchUpInside];
                     [menuButton setTag:index];
